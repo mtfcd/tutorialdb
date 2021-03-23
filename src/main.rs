45516f24cfd8b56a5cc3fd04f2fb3e4d5fd0e4b6
mtfcd <mtfcd@sina.com>
2021-03-23 18:18:59 +0800

    parse insert

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    io::{self, Write},
-    process,
-};
+use std::{cell::Ref, io::{self, Write}, process};
 
 fn main() {
     loop {
@@ -31,6 +28,10 @@ fn main() {
             PrepareResult::PrepareUnRecognizedStatement => {
                 println!("Unrecognized keyword at start of {}.", input);
                 continue;
+            },
+            PrepareResult::PrepareSyntaxError => {
+                println!("sytax error near {}.", input);
+                continue;
             }
         };
 
@@ -59,18 +60,23 @@ fn do_meta_command(input: &str) -> MetaCommandResult {
 }
 
 enum StatementType {
-    StatementInsert,
+    StatementInsert(Row),
     StatementSelect,
 }
 
 enum PrepareResult {
     PrepareSuccess(StatementType),
     PrepareUnRecognizedStatement,
+    PrepareSyntaxError,
 }
 
 fn prepare_statement(input: &str) -> PrepareResult {
     if input.starts_with("insert") {
-        return PrepareResult::PrepareSuccess(StatementType::StatementInsert);
+        let statement = match Row::new(input) {
+            Ok(row) => row,
+            Err(_) => return PrepareResult::PrepareSyntaxError
+        };
+        return PrepareResult::PrepareSuccess(StatementType::StatementInsert(statement));
     }
     if input == "select" {
         return PrepareResult::PrepareSuccess(StatementType::StatementSelect);
@@ -79,10 +85,50 @@ fn prepare_statement(input: &str) -> PrepareResult {
     return PrepareResult::PrepareUnRecognizedStatement;
 }
 
+#[derive(Debug)]
+struct Row {
+    id: i32,
+    username: [char; 25],
+    email: [char; 255]
+}
+
+impl Row {
+    fn new(input: &str) -> Result<Self, ()> {
+        let mut iter = input.split_ascii_whitespace();
+        iter.next(); // pop out "insert"
+
+        let id = match iter.next() {
+            Some(id_str) => match id_str.parse() {
+                Ok(value) => value,
+                Err(_) => return Err(())
+            },
+            None => return Err(())
+        };
+        let mut username = ['\0'; 25];
+        match iter.next() {
+            Some(name_str) => str2arr(name_str, &mut username),
+            None => return Err(())
+        };
+        let mut email = ['\0'; 255];
+        match iter.next() {
+            Some(mail_str) => str2arr(mail_str, &mut email),
+            None => return Err(())
+        };
+
+        return Ok(Row{id, username, email})
+    }
+}
+
+fn str2arr(s: &str, arr: &mut [char]) {
+    s.chars()
+    .zip(arr.iter_mut())
+    .for_each(|(b, ptr)| *ptr = b);
+} 
+
 fn execute_statement(statement: StatementType) {
     match statement {
-        StatementType::StatementInsert => {
-            println!("execute insert");
+        StatementType::StatementInsert(row) => {
+            println!("execute insert {:?}", row);
         }
         StatementType::StatementSelect => {
             println!("execute select");
